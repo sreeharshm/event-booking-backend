2a8e96343de17e6d5b40f3378a2d181b68adc3d2
sreeharsh <sree@gmail.com>
2026-07-24 12:38:20 +0530

    updated

--- event/settings.py
+++ event/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'event.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "event_booking",
+        "USER": "postgres",
+        "PASSWORD": "sree",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
